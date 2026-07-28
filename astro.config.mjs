7e5f7e133906c0f4db36b047f06ff34e87717626
Takaesu._.p <zinn.1019t@gmail.com>
2026-07-28 05:53:15 +0000

    Fix base path and add 404 fallback

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://System-Dev-Nagoshoko.github.io',
-  base: '/umipos.summit2026',
-  trailingSlash: 'always',
+  base: '/umipos.summit2026/',
   build: {
     format: 'directory'
   }
