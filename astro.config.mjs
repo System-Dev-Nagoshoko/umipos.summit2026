96b88f9987234307d9bc8edc0cc7a3e4f196b372
Takaesu._.p <zinn.1019t@gmail.com>
2026-09-10 00:56:32 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://System-Dev-Nagoshoko.github.io',
-  base: '/umipos.summit2026/',
+  site: 'https://uminosign.nagoyc.stki.org',
+  base: '/',
   build: {
     format: 'directory'
   }
